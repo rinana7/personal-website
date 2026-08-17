734c9301e3a2804e13671503979526f54b9e4cfd
rinana7 <sakairinana@gmail.com>
2026-08-16 19:49:49 -1000

    duplicate petals on screen

---
+++ script.js
@@ -0,0 +1,25 @@
+const borad = document.getElementById("petalBoard");
+const petalTemplate = borad.querySelector(".sakura");
+
+const positions = [
+    {x: 80, y:60, rotation: -20},
+    {x: 450, y:180, rotation: -35},
+    {x: 100, y:60, rotation: 20},
+    {x: 220, y:100, rotation: 15},
+    {x: 300, y:240, rotation: 70},
+    {x: 200, y:350, rotation: 25},
+];
+
+positions.forEach((position, index) => {
+    const petal = index === 0
+        ? petalTemplate
+        : petalTemplate.cloneNode(true);
+
+    petal.style.left =`${position.x}px`;
+    petal.style.top =`${position.y}py`;
+    petal.style.transform =`rotate(${position.rotation}deg)`;
+
+    petal.classList.add("draggable-petal");
+
+    borad.appendChild(petal);
+})
